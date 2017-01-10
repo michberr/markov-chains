b2f9097be1562ef23fcf52d66aec9dfc47af4345
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-10 12:12:42 -0800

    Accepts command line arguments for text file and ngram length

--- markov.py
+++ markov.py
@@ -1,4 +1,5 @@
 from random import choice
+import sys
 
 
 def open_and_read_file(file_path):
@@ -17,7 +18,7 @@ def open_and_read_file(file_path):
     return text
 
 
-def make_chains(text_string):
+def make_chains(text_string, ngrams):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -34,15 +35,15 @@ def make_chains(text_string):
     words = text_string.split()
     chains = {}
 
-    # get bi-grams starting at each word
-    for x in range(len(words[:-2])):
-        key = (words[x], words[x+1])
+    # get n-grams starting at each word
+    for x in range(len(words[:-ngrams])):
+        key = tuple(words[x:x + ngrams])
 
-        # add bi-grams to dictionary
+        # add n-grams to dictionary
         if key in chains:
-            chains[key].append(words[x+2])
+            chains[key].append(words[x + ngrams])
         else:
-            chains[key] = [words[x+2]]
+            chains[key] = [words[x + ngrams]]
 
     return chains
 
@@ -52,10 +53,10 @@ def make_text(chains):
 
     text = []
 
-    # Starting bigram (as tuple)
+    # Starting ngram (as tuple)
     start = choice(chains.keys())
 
-    # Add starting bigram to text list
+    # Add starting ngram to text list
     text.extend(start)
 
     while True:
@@ -65,8 +66,8 @@ def make_text(chains):
         # Add new word to text list
         text.append(new_word)
 
-        # Generate tuple for next bigram
-        new_key = (start[1], new_word)
+        # Generate tuple for next ngram
+        new_key = start[1:] + (new_word,)
 
         # Break out of loop if bigram doesn't exist
         if new_key in chains:
@@ -77,14 +78,14 @@ def make_text(chains):
     # Return text list formatted as string
     return ' '.join(text)
 
-
-input_path = "gettysburg.txt"
+input_path = sys.argv[1]
+ngrams = int(sys.argv[2])
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, ngrams)
 
 # Produce random text
 random_text = make_text(chains)
